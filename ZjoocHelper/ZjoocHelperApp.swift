6d2e7f1633215a0bd026e1ec18a2c9d7944debd9
ZacharyWin <2816813070@qq.com>
2022-04-20 21:58:40 +0800

    Initial Commit

---
+++ ZjoocHelper/ZjoocHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZjoocHelperApp.swift
+//  ZjoocHelper
+//
+//  Created by Zachary ╮ on 2022/4/20.
+//
+
+import SwiftUI
+
+@main
+struct ZjoocHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
